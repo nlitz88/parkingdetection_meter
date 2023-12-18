482b492f4c8698f962f77a0f707ab61e35f3f9bb
nlitz88 <nlitz88@gmail.com>
2023-12-18 03:33:04 -0500

    Added some HTTP client code from Espressif's library, but may just fall back on the normal HTTP library

--- parkingdetection_meter.ino
+++ parkingdetection_meter.ino
@@ -25,7 +25,7 @@
 #include "edge-impulse-sdk/dsp/image/image.hpp"
 
 #include "esp_camera.h"
-
+#include "esp_http_client.h"
 
 // Select camera model - find more camera models in camera_pins.h file here
 // https://github.com/espressif/arduino-esp32/blob/master/libraries/ESP32/examples/Camera/CameraWebServer/camera_pins.h
@@ -144,6 +144,137 @@ bool ei_camera_init(void);
 void ei_camera_deinit(void);
 bool ei_camera_capture(uint32_t img_width, uint32_t img_height, uint8_t *out_buf);
 
+#define MAX_HTTP_RECV_BUFFER 512
+#define MAX_HTTP_OUTPUT_BUFFER 2048
+static const char *TAG = "HTTP_CLIENT";
+
+esp_err_t _http_event_handler(esp_http_client_event_t *evt)
+{
+    static char *output_buffer;  // Buffer to store response of http request from event handler
+    static int output_len;       // Stores number of bytes read
+    switch(evt->event_id) {
+        case HTTP_EVENT_ERROR:
+            ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
+            break;
+        case HTTP_EVENT_ON_CONNECTED:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_CONNECTED");
+            break;
+        case HTTP_EVENT_HEADER_SENT:
+            ESP_LOGD(TAG, "HTTP_EVENT_HEADER_SENT");
+            break;
+        case HTTP_EVENT_ON_HEADER:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
+            break;
+        case HTTP_EVENT_ON_DATA:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+            // Clean the buffer in case of a new request
+            if (output_len == 0 && evt->user_data) {
+                // we are just starting to copy the output data into the use
+                memset(evt->user_data, 0, MAX_HTTP_OUTPUT_BUFFER);
+            }
+            /*
+             *  Check for chunked encoding is added as the URL for chunked encoding used in this example returns binary data.
+             *  However, event handler can also be used in case chunked encoding is used.
+             */
+            if (!esp_http_client_is_chunked_response(evt->client)) {
+                // If user_data buffer is configured, copy the response into the buffer
+                int copy_len = 0;
+                if (evt->user_data) {
+                    // The last byte in evt->user_data is kept for the NULL character in case of out-of-bound access.
+                    copy_len = MIN(evt->data_len, (MAX_HTTP_OUTPUT_BUFFER - output_len));
+                    if (copy_len) {
+                        memcpy(evt->user_data + output_len, evt->data, copy_len);
+                    }
+                } else {
+                    int content_len = esp_http_client_get_content_length(evt->client);
+                    if (output_buffer == NULL) {
+                        // We initialize output_buffer with 0 because it is used by strlen() and similar functions therefore should be null terminated.
+                        output_buffer = (char *) calloc(content_len + 1, sizeof(char));
+                        output_len = 0;
+                        if (output_buffer == NULL) {
+                            ESP_LOGE(TAG, "Failed to allocate memory for output buffer");
+                            return ESP_FAIL;
+                        }
+                    }
+                    copy_len = MIN(evt->data_len, (content_len - output_len));
+                    if (copy_len) {
+                        memcpy(output_buffer + output_len, evt->data, copy_len);
+                    }
+                }
+                output_len += copy_len;
+            }
+
+            break;
+        case HTTP_EVENT_ON_FINISH:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
+            if (output_buffer != NULL) {
+                // Response is accumulated in output_buffer. Uncomment the below line to print the accumulated response
+                // ESP_LOG_BUFFER_HEX(TAG, output_buffer, output_len);
+                free(output_buffer);
+                output_buffer = NULL;
+            }
+            output_len = 0;
+            break;
+        case HTTP_EVENT_DISCONNECTED:
+            ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
+            int mbedtls_err = 0;
+            esp_err_t err = esp_tls_get_and_clear_last_error((esp_tls_error_handle_t)evt->data, &mbedtls_err, NULL);
+            if (err != 0) {
+                ESP_LOGI(TAG, "Last esp error code: 0x%x", err);
+                ESP_LOGI(TAG, "Last mbedtls failure: 0x%x", mbedtls_err);
+            }
+            if (output_buffer != NULL) {
+                free(output_buffer);
+                output_buffer = NULL;
+            }
+            output_len = 0;
+            break;
+        case HTTP_EVENT_REDIRECT:
+            ESP_LOGD(TAG, "HTTP_EVENT_REDIRECT");
+            esp_http_client_set_header(evt->client, "From", "user@example.com");
+            esp_http_client_set_header(evt->client, "Accept", "text/html");
+            esp_http_client_set_redirection(evt->client);
+            break;
+    }
+    return ESP_OK;
+}
+
+static void http_rest_with_hostname_path(void) {
+  // esp_http_client_config_t config = {
+  //     .host = "192.168.0.151",
+  //     .port = 5000,
+  //     .path = "/process_plate",
+  //     .method = HTTP_METHOD_POST,
+  //     .transport_type = HTTP_TRANSPORT_OVER_TCP,
+  //     .event_handler = _http_event_handler,
+  // };
+
+  // Create new config for this client to connect to the parking HUB's API.
+  esp_http_client_config_t config;
+  esp_http_client_set_host(&config, "192.168.0.151");
+  esp_http_client_set_port(&config, 5000);
+  esp_http_client_set_path(&config, "/test_post");
+  esp_http_client_set_method(&config, HTTP_METHOD_POST);
+  esp_http_client_set_transport_type(&config, HTTP_TRANSPORT_OVER_TCP);
+  esp_http_client_set_event_handler(&config, _http_event_handler)
+
+  // Create a handle for the config to reference this HTTP connection.
+  esp_http_client_handle_t client = esp_http_client_init(&config);
+
+  // POST
+  // TODO: Update this by setting this to the address of the buffer our image data is in!
+  const char *post_data = "field1=value1&field2=value2";
+  // Update the address where the client code should send the data from.
+  esp_http_client_set_post_field(client, post_data, strlen(post_data));
+  err = esp_http_client_perform(client);
+  if (err == ESP_OK) {
+      ESP_LOGI(TAG, "HTTP POST Status = %d, content_length = %"PRId64,
+              esp_http_client_get_status_code(client),
+              esp_http_client_get_content_length(client));
+  } else {
+      ESP_LOGE(TAG, "HTTP POST request failed: %s", esp_err_to_name(err));
+}
+
 /**
 * @brief      Arduino setup function
 */
@@ -184,6 +315,9 @@ void loop()
     if (ei_sleep(500) != EI_IMPULSE_OK) {
         return;
     }
+
+    // Try out sending a POST request to the server.
+    http_rest_with_hostname_path();
 
     // Serial.println("Loop ran!");
 
